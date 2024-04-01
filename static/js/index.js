483b845ed0ed17db9d96b340f2ea8c4e9422a499
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2024-04-01 17:59:17 +0700

    validate-2

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,19 @@
 function validateInput() {
   var secretKey = document.getElementById("secretKey").value;
   var keyLength = parseInt(document.getElementById("keyLength").value);
+  var plainText = document.getElementById("plainText").value;
+  var cipherText = document.getElementById("cipherText").value;
 
   // Kiểm tra xem Secret Key có được nhập hay không
   if (secretKey === "") {
-    //   alert("Please enter a Secret Key.");
     document.getElementById("error-message").innerHTML = "Please enter a Secret Key.";
     document.getElementById("error-message").style.display = "block";
     document.getElementById("error-message").style.color = "red";
     return false;
   }
 
-  // Kiểm tra xem Length of Key có được chọn hay không
-  // if (isNaN(keyLength)) {
-  //   // alert("Please select a Length of Key.");
-  //   document.getElementById("error-message").innerHTML = "Please select a Length of Key.";
-  //   document.getElementById("error-message").style.display = "block";
-  //   document.getElementById("error-message").style.color = "red";
-  //   return false;
-  // }
-
   // Kiểm tra xem Length of Key có hợp lệ hay không (128, 192, hoặc 256)
   if (![128, 192, 256].includes(keyLength)) {
-      // alert("Invalid Length of Key. Please select 128, 192, or 256.");
     document.getElementById("error-message").innerHTML = "Invalid Length of Key. Please select 128, 192, or 256.";
     document.getElementById("error-message").style.display = "block";
     document.getElementById("error-message").style.color = "red";
@@ -34,8 +25,15 @@ function validateInput() {
 
   // Kiểm tra xem độ dài của Secret Key có tương ứng với Length of Key hay không
   if (secretKeyLength !== keyLength) {
-    //   alert("The length of the Secret Key does not match the selected Length of Key.");
     document.getElementById("error-message").innerHTML = "The length of the Secret Key does not match the selected Length of Key.";
+    document.getElementById("error-message").style.display = "block";
+    document.getElementById("error-message").style.color = "red";
+    return false;
+  }
+
+  //nếu đã nhập plaintext thì không cần nhập ciphertext
+  if (plainText === "" && cipherText === "") {
+    document.getElementById("error-message").innerHTML = "Please enter either Plain Text or Cipher Text.";
     document.getElementById("error-message").style.display = "block";
     document.getElementById("error-message").style.color = "red";
     return false;
